e0146fba64898ec68c8d13b58612fea1e0600af5
Nikola Jovanovic <jovanovicn.96@gmail.com>
2021-11-02 22:59:52 +0100

    Fixup car dynamics tracker

--- nikola/car_dynamics_tracker.py
+++ nikola/car_dynamics_tracker.py
@@ -15,7 +15,7 @@ class CarDynamicsTracker:
     def max_cornering_speed(self):
         raise NotImplementedError
 
-    def accel_action(self, current_speed, action: Action):
+    def estimate_next_speed(self, current_speed, action: Action):
         assert action in CarDynamicsTracker.ACCEL_ACTIONS
         return current_speed
 
@@ -25,17 +25,17 @@ class CarDynamicsTracker:
 
 class RookieCarDynamicsTracker(CarDynamicsTracker):
 
-    INC_LOW =  [0.0, 1.0]
-    INC_HIGH = [300.0, 301.0]
-    DEC_LOW = [1.0, 0.0]
-    DEC_HIGH = [301.0, 300.0]
+    INC_LOW =  [-1.0, 0.0]
+    INC_HIGH = [600.0, 601.0]
+    DEC_LOW = [0.0, -1.0]
+    DEC_HIGH = [601.0, 600.0]
 
     def __init__(self):
         self.data = {
             drs_active: {
                 Action.FullThrottle: [RookieCarDynamicsTracker.INC_LOW, RookieCarDynamicsTracker.INC_HIGH],
                 Action.LightThrottle: [RookieCarDynamicsTracker.INC_LOW, RookieCarDynamicsTracker.INC_HIGH],
-                Action.HeavyBrake: [[RookieCarDynamicsTracker.DEC_LOW, RookieCarDynamicsTracker.DEC_HIGH]],
+                Action.HeavyBrake: [RookieCarDynamicsTracker.DEC_LOW, RookieCarDynamicsTracker.DEC_HIGH],
                 Action.LightBrake: [RookieCarDynamicsTracker.DEC_LOW, RookieCarDynamicsTracker.DEC_HIGH]
             } for drs_active in [False, True]
         }
@@ -46,20 +46,13 @@ class RookieCarDynamicsTracker(CarDynamicsTracker):
 
         if action in CarDynamicsTracker.ACCEL_ACTIONS:
             current_data = self.data[previous_car_state.drs_active][action]
-            # Remove initial values
-            if previous_car_state.speed < 5.0:
-                current_data.remove(RookieCarDynamicsTracker.INC_LOW)
-                current_data.remove(RookieCarDynamicsTracker.DEC_LOW)
-            elif previous_car_state.speed > 200.0:
-                current_data.remove(RookieCarDynamicsTracker.INC_HIGH)
-                current_data.remove(RookieCarDynamicsTracker.DEC_HIGH)
             # Remove closest value if too close
             distances = np.abs(np.array(current_data)[:, 0] - previous_car_state.speed)
             closest_id = np.argmin(distances)
             if distances[closest_id] < 1.0:
                 del current_data[closest_id]
             # Add the new record
-            current_data.append([[previous_car_state.speed, new_car_state.speed]])
+            current_data.append([previous_car_state.speed, new_car_state.speed])
 
         elif action in [Action.TurnLeft, Action.TurnRight]:
             if previous_car_state.heading == new_car_state.heading:
@@ -79,10 +72,10 @@ class RookieCarDynamicsTracker(CarDynamicsTracker):
     def max_cornering_speed(self):
         return sum(self.cornering_speed_bounds) / 2.0
 
-    def accel_action(self, current_speed, action: Action, drs_active: bool):
-        super().accel_action(current_speed, action)
-        current_data = self.data[drs_active][action]
-        assert len(current_data) > 2
+    def estimate_next_speed(self, current_speed, action: Action, drs_active: bool):
+        super().estimate_next_speed(current_speed, action)
+        current_data = np.array(self.data[drs_active][action])
+        assert len(current_data) >= 2
         interpolation = scipy.interpolate.interp1d(
             current_data[:, 0], current_data[:, 1], fill_value='extrapolate', assume_sorted=False)
         return interpolation(current_speed)

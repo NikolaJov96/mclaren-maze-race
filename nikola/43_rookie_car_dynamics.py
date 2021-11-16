ab336649e52558050d53a653f39e07a34771a659
NikolaJov96 <jovanovicn.96@gmail.com>
2021-11-17 00:00:38 +0100

    Rookie car dynamic inspection

--- nikola/43_rookie_car_dynamics.py
+++ nikola/43_rookie_car_dynamics.py
@@ -6,25 +6,45 @@ from imports import *
 from drivers.rookiedriver import RookieDriver
 from nikola.race_logger import RaceLogger
 from nikola.car_dynamics_tracker import RookieCarDynamicsTracker
+from nikola.straight_simulator import StraightSimulator
 
 
 class RookieDriverCarDynamics(RookieDriver):
 
-    def __init__(self, name, race_logger_dir, car_dynamics_tracker: RookieCarDynamicsTracker, *args, **kwargs):
+    def __init__(self, name,
+            race_logger_dir,
+            car_dynamics_tracker: RookieCarDynamicsTracker,
+            use_target_speeds: bool,
+            *args, **kwargs):
         super().__init__(name, *args, **kwargs)
 
         self.race_logger_dir = race_logger_dir
         self.car_dynamics_tracker = car_dynamics_tracker
+        self.use_target_speeds = use_target_speeds
         self.race_id = 0
         self.race_logger = None
 
-        self.safety_car_target_speed = 0
+        self.potential_stop = False
 
     def prepare_for_race(self):
         self.race_id += 1
         if self.race_logger_dir != '':
             self.race_logger = RaceLogger(os.path.join(self.race_logger_dir, '{}.png'.format(self.race_id)))
+        self.potential_stop = False
         return super().prepare_for_race()
+
+    def _get_target_speed(self, distance_ahead, safety_car_active, target_speeds=None):
+        if distance_ahead == 0:
+            return 0
+        if self.use_target_speeds:
+            a = super()._get_target_speed(distance_ahead, safety_car_active, target_speeds=target_speeds)
+            target_speed = StraightSimulator.get_target_speeds(self.car_dynamics_tracker, distance_ahead, self.potential_stop)[-1]
+            if safety_car_active:
+                target_speed = min(target_speed, self.safety_car_speed)
+            target_speed = min(target_speed, 350.0)
+            return target_speed
+        else:
+            return super()._get_target_speed(distance_ahead, safety_car_active, target_speeds=target_speeds)
 
     def estimate_next_speed(self, action: Action, speed, drs_active: bool, **kwargs):
         if action == Action.Continue:
@@ -37,7 +57,12 @@ class RookieDriverCarDynamics(RookieDriver):
             self.race_logger.log_race_step(
                 previous_car_state, previous_track_state, action,
                 new_car_state, new_track_state, result)
-        self.car_dynamics_tracker.add_data_point(action, previous_car_state, new_car_state)
+        self.car_dynamics_tracker.add_data_point(action, previous_car_state, new_car_state, result)
+        if previous_track_state.distance_ahead == 0 and new_track_state.distance_ahead > 0:
+            if previous_track_state.distance_left > 0 and previous_track_state.distance_right > 0:
+                self.potential_stop = True
+            else:
+                self.potential_stop = False
         return super().update_with_action_results(
             previous_car_state, previous_track_state, action,
             new_car_state, new_track_state, result)
@@ -50,21 +75,51 @@ if __name__ == '__main__':
         os.mkdir(path)
 
     # Run one season with turn logger
-    print('Running season')
-    Season(Level.Rookie).race(
-        RookieDriverCarDynamics(
-            'RDCD',
-            os.path.join(path, 'season'),
-            RookieCarDynamicsTracker()))
+    for use_target_speeds in [False, True]:
+        print('Running season with target speeds {}'.format(use_target_speeds))
+        Season(Level.Rookie).race(
+            RookieDriverCarDynamics(
+                'RDCD',
+                os.path.join(path, 'season_{}'.format(use_target_speeds)),
+                RookieCarDynamicsTracker(),
+                use_target_speeds))
 
     # Run the championship with the original rookie
-    print('Running championship')
+    print('Running championship with no target speeds')
     drivers = [
         RookieDriver('RD'),
-        RookieDriverCarDynamics('RDCD', '', RookieCarDynamicsTracker())
+        RookieDriverCarDynamics('RDCD', '', RookieCarDynamicsTracker(), False)
     ]
     championship = Championship(drivers, Level.Rookie, shuffle_tracks=True, verbose=True)
-    championship_results, _, _ = championship.run_championship(num_repeats=1000)
+    championship_results, finishing_positions, all_race_times = championship.run_championship(num_repeats=10)
     plot_multiple_championship_results(championship_results)
-    plt.savefig(os.path.join(path, 'championship.png'))
+    plt.savefig(os.path.join(path, 'championship_no_target_speeds.png'))
     plt.close()
+
+    # Run the championship with the original rookie
+    print('Running championship with target speeds')
+    drivers = [
+        RookieDriver('RD'),
+        RookieDriverCarDynamics('RDCD', '', RookieCarDynamicsTracker(), True)
+    ]
+    championship = Championship(drivers, Level.Rookie, shuffle_tracks=True, verbose=True)
+    championship_results, finishing_positions, all_race_times = championship.run_championship(num_repeats=10)
+    plot_multiple_championship_results(championship_results)
+    plt.savefig(os.path.join(path, 'championship_target_speeds.png'))
+    plt.close()
+
+    won_races = finishing_positions['RD'] < finishing_positions['RDCD']
+    print('won_races')
+    print(won_races)
+    won_time_diff = all_race_times['RDCD'][won_races] - all_race_times['RD'][won_races]
+    print('won_time_diff')
+    print(won_time_diff)
+    print(sum(won_time_diff) / len(won_time_diff))
+
+    lost_races = finishing_positions['RD'] > finishing_positions['RDCD']
+    print('lost_races')
+    print(lost_races)
+    lost_time_diff = all_race_times['RD'][lost_races] - all_race_times['RDCD'][lost_races]
+    print('lost_time_diff')
+    print(lost_time_diff)
+    print(sum(lost_time_diff) / len(lost_time_diff))

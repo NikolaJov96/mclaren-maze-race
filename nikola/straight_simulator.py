16cae4886507c0b8fe05b9e82a7f2954c1702224
NikolaJov96 <jovanovicn.96@gmail.com>
2021-11-20 01:59:44 +0100

    Straight simulator comments

--- nikola/straight_simulator.py
+++ nikola/straight_simulator.py
@@ -4,6 +4,9 @@ from nikola.car_dynamics_tracker import CarDynamicsTracker
 
 
 class StraightSimulator:
+    """
+    Deals with predicting future
+    """
 
     def __init__(self):
         pass
@@ -14,8 +17,11 @@ class StraightSimulator:
             return np.zeros(1)
 
         target_speeds = np.zeros(distance_ahead + 1)
+        # Final speed, for imaginary distance_ahead = -1
         target_speeds[0] = 0
+        # Max conering speed for distance_ahead = 0
         target_speeds[1] = car_dynamics_tracker.max_cornering_speed()
+        # In case that we may need to stop at the end of the straight, invalidate the max cornering speed
         for i in range(1 if potential_stop else 2, distance_ahead + 1):
             # Binary search for the optimal speed
             bounds = [target_speeds[i - 1], target_speeds[i - 1] + 100.0]
@@ -41,13 +47,18 @@ class StraightSimulator:
             potential_stop: bool,
             drs_active: bool,
             safety_car_speed: float = 0.0):
+        # Find optimal actions to complete the straight
         speeds = np.zeros(distance_ahead)
+        target_speeds = StraightSimulator.get_target_speeds(car_dynamics_tracker, distance_ahead, potential_stop)
         break_target_speed = False
         for d in range(distance_ahead):
-            target_speed = StraightSimulator.get_target_speeds(car_dynamics_tracker, distance_ahead, potential_stop)[-1]
+            # Find step target speed
+            target_speed = target_speeds[-d - 1]
             if safety_car_speed > 0.1:
                 target_speed = min(target_speed, safety_car_speed)
+            # Find the best action we can manage
             action = car_dynamics_tracker.choose_best_action(speed, target_speed, drs_active)
+            # Simulate the action
             speeds[d] = car_dynamics_tracker.estimate_next_speed(speed, action, drs_active)
             speed = speeds[d]
             break_target_speed |= speed > target_speed
